b09147e5f56369a870f900e7e1f866596a1e286b
Ângelo Costa Neto <angelo_costa_neto@outlook.com>
2017-11-17 01:35:31 -0200

    adicionado o tenmplate

--- resources/views/realstates/index.blade.php
+++ resources/views/realstates/index.blade.php
@@ -1,35 +1,41 @@
+@extends('layouts.app')
 
-<table>
-    <tr>
-        <th>Id</th>
-        <th>Razão Social</th>
-        <th>Nome Fantasia</th>
-        <th>CNPJ</th>
-        <th>CRECI</th>
-        <th>CEP</th>
-        <th>Logradouro</th>
-        <th>Bairro</th>
-        <th>Ações</th>
-    </tr>
-
-    @foreach ($realStates as $realState)
-    <tr>
-        <td>{{ $realState->id }}</td>
-        <td>{{ $realState->company }}</td>
-        <td>{{ $realState->trading_name }}</td>
-        <td>{{ $realState->cnpj }}</td>
-        <td>{{ $realState->creci }}</td>
-        <td>{{ $realState->address->cep }}</td>
-        <td>{{ $realState->address->street }}</td>
-        <td>{{ $realState->address->district }}</td>
-        <td><a href="{{ action('RealStatesController@edit', $realState->id) }}">Edit</a></td>
-        <td>
-            <form action="{{ action('RealStatesController@destroy', $realState) }}" method="POST">
-                {{csrf_field()}}
-                <input name="_method" type="hidden" value="DELETE">
-                <button class="btn btn-danger" type="submit">Delete</button>
-            </form>
-        </td>
-    </tr>
-    @endforeach
-</table>
+@section('content')
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Razão Social</th>
+                <th>Nome Fantasia</th>
+                <th>CNPJ</th>
+                <th>CRECI</th>
+                <th>CEP</th>
+                <th>Logradouro</th>
+                <th>Bairro</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($realStates as $realState)
+            <tr>
+                <td>{{ $realState->id }}</td>
+                <td>{{ $realState->company }}</td>
+                <td>{{ $realState->trading_name }}</td>
+                <td>{{ $realState->cnpj }}</td>
+                <td>{{ $realState->creci }}</td>
+                <td>{{ $realState->address->cep }}</td>
+                <td>{{ $realState->address->street }}</td>
+                <td>{{ $realState->address->district }}</td>
+                <td><a class="btn btn-info" href="{{ action('RealStatesController@edit', $realState->id) }}">Editar</a></td>
+                <td>
+                    <form action="{{ action('RealStatesController@destroy', $realState) }}" method="POST">
+                        {{csrf_field()}}
+                        <input name="_method" type="hidden" value="DELETE">
+                        <button class="btn btn-danger" type="submit">Deletar</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
--- resources/views/residences/create.blade.php
+++ resources/views/residences/create.blade.php
@@ -1,49 +1,113 @@
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div><br />
-@endif
- @if (\Session::has('success'))
-    <div class="alert alert-success">
-        <p>{{ \Session::get('success') }}</p>
-    </div><br />
-@endif
-<form method="post" action="{{url('residences')}}">
-    <label>title</label>
-    <input type="text" name="title">
-    <label>residence type</label>
-    <select name="residences_type">
-        <option>Selecione um tipo</option>
-        @foreach ($residencesTypes as $key => $residencesType)
-            <optgroup label="{{$key}}">
-                @foreach ($residencesType as $rT)
-                    <option value="{{$rT['id']}}">{{$rT['name']}}</option>
+@extends('layouts.app')
+
+@section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
                 @endforeach
-            </optgroup>
-        @endforeach
-    </select>
-    <label>description</label>
-    <input type="text" name="description">
-    <label>negotiation_price</label>
-    <input type="text" name="negotiation_price">
-    <label>toilet</label>
-    <input type="number" name="toilet">
-    <label>bathroom</label>
-    <input type="number" name="bathroom">
-    <label>suite</label>
-    <input type="number" name="suite">
-    <label>garage</label>
-    <input type="number" name="garage">
-    <label>area</label>
-    <input type="number" name="area" step="0.01">
-    <label>cep</label>
-    <input type="text" name="cep">
-    <label>numero</label>
-    <input type="text" name="number">
-    <input type="submit" value="salvar">
-    {{csrf_field()}}
-</form>
+            </ul>
+        </div><br />
+    @endif
+     @if (\Session::has('success'))
+        <div class="alert alert-success">
+            <p>{{ \Session::get('success') }}</p>
+        </div><br />
+    @endif
+    <form method="post" class="form-horizontal form-label-left" action="{{url('residences')}}">
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Titulo</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="text" name="title">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Tipo da residencia</label>
+             <div class="col-md-6 col-sm-6 col-xs-12">
+                <select class="form-control col-md-7 col-xs-12" name="residences_type">
+                    <option>Selecione um tipo</option>
+                    @foreach ($residencesTypes as $key => $residencesType)
+                        <optgroup label="{{$key}}">
+                            @foreach ($residencesType as $rT)
+                                <option value="{{$rT['id']}}">{{$rT['name']}}</option>
+                            @endforeach
+                        </optgroup>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Descrição</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="text" name="description">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Preço</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="text" name="negotiation_price">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Toilet</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="number" name="toilet">
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Banheiros</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="number" name="bathroom">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Suites</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="number" name="suite">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Garagem</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="number" name="garage">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Área</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="number" name="area" step="0.01">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">CEP</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="text" name="cep">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Número</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input class="form-control col-md-7 col-xs-12" type="text" name="number">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                <button type="submit" class="btn btn-success">Enviar</button>
+            </div>
+        </div>
+        
+        {{csrf_field()}}
+    </form>
+@endsection
