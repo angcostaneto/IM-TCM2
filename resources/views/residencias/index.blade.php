745d75c20259657cbf188c655c93fa49ea33137c
Ângelo Costa Neto <angelo_costa_neto@outlook.com>
2018-01-08 20:52:37 -0200

    corrigie o layout

--- resources/views/residencias/index.blade.php
+++ resources/views/residencias/index.blade.php
@@ -14,50 +14,55 @@
             </div>
             <div class="x_content">
                 <table class="table table-hover">
-                    <tr>
-                        <th>Codigo</th>
-                        <th>Titulo</th>
-                        <th>Descrição</th>
-                        <th>Tipo</th>
-                        <th>Preço</th>
-                        <th>Quartos</th>
-                        <th>Toilet</th>
-                        <th>Banheiro</th>
-                        <th>Suite</th>
-                        <th>Garagem</th>
-                        <th>Área (m²)</th>
-                        <th>CEP</th>
-                        <th>Logradouro</th>
-                        <th>Bairro</th>
-                        <th>Ações</th>
-                    </tr>
-
-                    @foreach ($residencias as $residencia)
-                    <tr>
-                        <td>{{ $residencia->codigo }}</td>
-                        <td>{{ $residencia->header_anuncio }}</td>
-                        <td>{{ $residencia->descricao }}</td>
-                        <td>{{ $residencia->tipo->nome }}</td>
-                        <td>{{ $residencia->preco }}</td>
-                        <td>{{ $residencia->quartos }}</td>
-                        <td>{{ $residencia->toilets }}</td>
-                        <td>{{ $residencia->banheiros }}</td>
-                        <td>{{ $residencia->suites }}</td>
-                        <td>{{ $residencia->garagens }}</td>
-                        <td>{{ $residencia->area }}</td>
-                        <td>{{ $residencia->endereco->cep }}</td>
-                        <td>{{ $residencia->endereco->rua }}</td>
-                        <td>{{ $residencia->endereco->bairro }}</td>
-                        <td>
-                            <a class="btn btn-info" href="{{ action('ResidenciasController@edit', $residencia->id) }}">Editar</a>
-                            <form action="{{ action('ResidenciasController@destroy', $residencia) }}" method="POST">
-                                {{csrf_field()}}
-                                <input name="_method" type="hidden" value="DELETE">
-                                <button class="btn btn-danger" type="submit">Deletar</button>
-                            </form>
-                        </td>
-                    </tr>
-                    @endforeach
+                    <thead>
+                        <tr>
+                            <th>Codigo</th>
+                            <th>Titulo</th>
+                            <th>Descrição</th>
+                            <th>Tipo</th>
+                            <th>Preço</th>
+                            <th>Quartos</th>
+                            <th>Toilet</th>
+                            <th>Banheiro</th>
+                            <th>Suite</th>
+                            <th>Garagem</th>
+                            <th>Área (m²)</th>
+                            <th>CEP</th>
+                            <th>Logradouro</th>
+                            <th>Bairro</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($residencias as $residencia)
+                        <tr>
+                            <td>{{ $residencia->codigo }}</td>
+                            <td>{{ $residencia->header_anuncio }}</td>
+                            <td>{{ $residencia->descricao }}</td>
+                            <td>{{ $residencia->tipo->nome }}</td>
+                            <td>{{ $residencia->preco }}</td>
+                            <td>{{ $residencia->quartos }}</td>
+                            <td>{{ $residencia->toilets }}</td>
+                            <td>{{ $residencia->banheiros }}</td>
+                            <td>{{ $residencia->suites }}</td>
+                            <td>{{ $residencia->garagens }}</td>
+                            <td>{{ $residencia->area }}</td>
+                            <td>{{ $residencia->endereco->cep }}</td>
+                            <td>{{ $residencia->endereco->rua }}</td>
+                            <td>{{ $residencia->endereco->bairro }}</td>
+                            <td>
+                                <div class="inline">
+                                    <a class="btn btn-info" href="{{ action('ResidenciasController@edit', $residencia->id) }}">Editar</a>
+                                    <form action="{{ action('ResidenciasController@destroy', $residencia) }}" method="POST">
+                                        {{csrf_field()}}
+                                        <input name="_method" type="hidden" value="DELETE">
+                                        <button class="btn btn-danger" type="submit">Deletar</button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
                 </table>
             </div>
         </div>
