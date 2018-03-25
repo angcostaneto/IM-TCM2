c2efae38c5b85d63a35a0e2fafb0515f680fe7d6
VictorLM <victordinami@gmail.com>
2018-03-25 09:46:47 -0300

    ...

--- resources/views/cliente/cliente.blade.php
+++ resources/views/cliente/cliente.blade.php
@@ -1,119 +1,28 @@
 @extends ('cliente.template')
 
-@section('titulo', 'Apperitivo')
-
 @section('content')
 
-      </div>
-    </div>
-    <div class="navbar navbar-inverse bg-inverse">
-      <div class="container d-flex justify-content-between">
-        <a href="#" class="navbar-brand"><img src="\img\logo-branco.png"></a>
-        <div>
-            <a href="{{url('home')}}" class="navbar-brand">
-                <i class="ion-locked"></i>
-            Login</a>
-        </div>
-      </div>
-        
-    </div>
+    <div class="row">
 
-    <section class="jumbotron text-center lightgreybg">
+        <form>
 
-        <br/>
-        <form action="{{action('ClienteController@procurar')}}" method="post">
-            
-            {{ csrf_field() }}
-            
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div><br />
-            @endif
-            
-            <div class="formzao mx-auto">
-                
-                <div class="tipoimo btn-group btn-group-justified">
-                    <input type="button" value="Alugar" class="btn btn-danger tiponegocio">
-                    <input type="button" value="Comprar" class="btn btn-success tiponegocio">
-                    <input type="button" value="Vender" class="btn btn-info tiponegocio">
-                    <input type="hidden" value="" id="tiponegocio" name="tiponegocio">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <button class="btn btn-outline-primary" type="button">COMPRAR</button>
+                    <button class="btn btn-outline-secondary" type="button">ALUGAR</button>
                 </div>
+                <input type="text" class="form-control" aria-label="Large" aria-describedby="inputGroup-sizing-sm" placeholder="CIDADE">
 
-                <div class="tipo">
-                    <select name="tipoimovel" class="form-control">
-                        <option value="">Qual tipo de imóvel</option>
-                        <option value="Casa de Vila">Casa de Vila</option>
-                        <option value="Casa de Condomínio">Casa de Condomínio</option>
-                        <option value="Apartamento Padrão">Apartamento Padrão</option>
-                        <option value="Cobertura">Cobertura</option>
-                        <option value="Kitnet">Kitnet</option>
-                        <option value="Chácara">Chácara</option>
-                        <option value="Fazenda">Fazenda</option>
-                        <option value="Sitio">Sitio</option>
-                    </select>
-                </div>
-
-                <div class="cidade">
-                    <input type="text" name="cidade" class="form-control" maxlength="30" placeholder="Cidade...">
-                </div>
-                
-                <div class="procurar">
-                    <button type="submit" style="cursor: pointer;" class="btn btn-primary">Procurar</button>
-                </div>
-                
+                <select class="custom-select" id="inputGroupSelect01">
+                    <option selected>Choose...</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
             </div>
+
         </form>
 
-    </section>
+    </div>
 
-    <div class="album text-muted">
-      <div class="container">
-
-        <div class="row">
-            
-            @if($residencias->count() > 0)
-            
-                @foreach($residencias as $residencia)
-                <div class="col-md-4">
-                    <div id="residences">
-                        <img class="foto-imovel" alt="Foto da residência" src="@if(count(json_decode($residencia->imagem))>0){{json_decode($residencia->imagem)[0]}}@else http://support.yumpu.com/en/wp-content/themes/qaengine/img/default-thumbnail.jpg @endif">
-                        <h3 class="card-title">{{$residencia->header_anuncio}}</h3>
-                        <span class="badge badge-success">{{$residencia->tipo->nome}}</span>
-                        <p class="card-text">{{$residencia->descricao}}</p>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item"><strong>Valor:&nbsp;</strong>R${{$residencia->preco}}</li>
-                            <li class="list-group-item small text-left">
-                                <strong>Área:&nbsp;</strong>{{$residencia->area}} m² | 
-                                <strong>&nbsp;Quartos:&nbsp;</strong>{{$residencia->quartos}} | 
-                                <strong>&nbsp;Banheiros:&nbsp;</strong>{{$residencia->banheiros}} | 
-                                <strong>&nbsp;Suites:&nbsp;</strong>
-                                    @if(!empty($residencia->suites))
-                                        {{$residencia->suites}}
-                                    @else 
-                                        0
-                                    @endif | 
-                                <strong>&nbsp;Vagas na garagem:&nbsp;</strong>{{$residencia->garagens}}
-                            </li>
-                            <li class="list-group-item"><strong>Endereço:&nbsp;</strong>
-                                {{ $residencia->endereco->rua }}, {{ $residencia->endereco->numero }}, 
-                                {{ $residencia->endereco->bairro }}, {{ $residencia->endereco->cidade }}.
-                            </li>
-                        </ul>
-                        <a href="#" class="btninfo btn btn-primary">Mais informações</a>
-                    </div>
-                </div>
-                @endforeach
-            
-            </div>
-        {!! $residencias->links() !!}
-    @else
-        <h1>Nenhum resultado encontrado.</h1>
-        </div>
-    @endif
-    
-@stop
+@endsection
--- resources/views/cliente/template.blade.php
+++ resources/views/cliente/template.blade.php
@@ -1,38 +1,65 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="pt-BR">
     <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>{{$title or 'Home'}}</title>
         <!--Bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
-        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
         <link rel="stylesheet" href="{{url('css/client.css')}}"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-        
-        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
-        <script src="{{url('js/client.js')}}"></script>
-            
+        @stack('styles')
     </head>
 
     <body>
-        <div class="collapse bg-inverse" id="navbarHeader">
+
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4 box-shadow">
             <div class="container">
-                @yield('content')
+                <a class="navbar-brand" href="#">
+                    <img src="\img\logo-branco.png" class="img-fluid" alt="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarCollapse">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                        </li>
+                    </ul>
+                    <div class="mt-2 mt-md-0">
+                        @if(Auth::guest())
+                            <a href="/home">
+                                <button type="button" class="btn btn-outline-success btn-lg">ANUNCIE SEU IMÓVEL</button>
+                            </a>
+                        @else
+                            <a href="/home">Minha conta</a>
+                        @endif
+                    </div>
+                </div>
             </div>
+        </nav>
+
+        <div class="container">
+            @yield('content')
         </div>
         
-        <footer class="text-muted">
-            <div class="container">
-              <p class="float-right">
-                <a href="#"><button type="button" class="btn btn-primary">Contato</button></a>
-                <a href="#"><button type="button" id="sobrebtn" class="btn btn-primary">Sobre</button></a>
-                <a href="#">Voltar ao topo <span class='ion-chevron-up'></span></a>
-              </p>
-              <p>Apperitivo Ltda. &copy; Feito com Bootstrap.</p>
+        <hr/>
+
+        <footer class="footer bg-light">
+            <div class="container text-muted">
+                <p class="float-right">
+                    <a href="#"><button type="button" class="btn btn-primary">Contato</button></a>
+                    <a href="#"><button type="button" id="sobrebtn" class="btn btn-primary">Sobre</button></a>
+                    <a href="#">Voltar ao topo <span class='ion-chevron-up'></span></a>
+                </p>
+                <p>Apperitivo Ltda. &copy; Feito com Bootstrap.</p>
             </div>
         </footer>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+        <script src="{{url('js/client.js')}}"></script>
+        @stack('scripts')
         
     </body>
 
