2d2d922bd32070caa0094b80484bd9b82b92e4aa
Angelo Costa Neto <angelo_costa_neto@outlook.com>
2018-06-20 18:56:44 -0300

    Remove informações

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -19,13 +19,7 @@
                         <th>Nome</th>
                         <th>E-mail</th>
                         <th>Tipo</th>
-                        <th>RG</th>
                         <th>CPF</th>
-                        <th>CEP</th>
-                        <th>Logradouro</th>
-                        <th>Número</th>
-                        <th>Bairro</th>
-                        <th>Cidade/Estado</th>
                         <th>Ações</th>
                     </tr>
 
@@ -35,16 +29,7 @@
                         <td>{{ $user->name }}</td>
                         <td>{{ $user->email }}</td>
                         <td>{{ $user->tipo }}</td>
-                        <td>{{ $user->rg }}</td>
                         <td>{{ $user->cpf }}</td>
-                        <td>@if (!empty($user->endereco->cep)){{$user->endereco->cep}} @endif</td>
-                        <td>@if (!empty($user->endereco->rua)){{$user->endereco->rua}} @endif</td>
-                        <td>@if (!empty($user->endereco->numero)){{$user->endereco->numero}} @endif</td>
-                        <td>@if (!empty($user->endereco->bairro)){{$user->endereco->bairro}} @endif</td>
-                        <td>
-                            @if (!empty($user->endereco->cidade)){{$user->endereco->cidade}} @endif
-                            @if (!empty($user->endereco->estado)){{$user->endereco->estado}} @endif
-                        </td>
                         <td>
                             <div class="inline">
                                 <a class="btn btn-small btn-info" href="{{ action('Auth\RegisterController@edit', $user->id) }}">Editar</a>
